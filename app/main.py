465555e6cc856911a2d6b1e598d884ca72163de9
LilShiny <blagovestdoukovy2@gmail.com>
2025-11-12 17:42:55 +0100

    feat: add content to main and users router

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
 # main py
+
+from fastapi import FastAPI
+
+from app.routers import users
+
+app = FastAPI(debug=True)
+app.include_router(users.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to PManager API"}
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,1 +1,71 @@
 # users router
+
+from pydantic import BaseModel
+from dataclasses import dataclass
+from fastapi import APIRouter, status, HTTPException
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+class UserBase(BaseModel):
+    nickname:str
+    password:str
+
+class UserIn(UserBase):
+    name:str
+
+class UserDb(UserIn):
+    id:int
+
+class UserLogin(UserBase):
+    pass
+
+users:list[UserDb] = []
+
+class TokenOut(BaseModel):
+    token:str
+
+@router.post("/signup/", status_code=status.HTTP_201_CREATED)
+async def create_user(userIn:UserIn):
+    in len(usersFound) > 0:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Username already exists"
+        )
+
+    users.append(
+        UserDb(
+            id=len(users)+1,
+            name=userIn.name,
+            nickname=userIn.nickname,
+            password=userIn.password
+        )
+    )
+
+@router.post(
+    "/auth/",
+    response_model=TokenOut,
+    status_code=status.HTTP_200_OK
+)
+async def login(userLoginIn:UserLogin):
+    usersFound = [u for u in users if u.username == userLoginIn.username]
+    if not usersFound:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username and/or password incorrect"
+        )
+    user: UserDb=usersFound[0]
+
+    if user.password != userLoginIn.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Username and/or password incorrect"
+        )
+
+    return TokenOut(
+        token=f"mytoken:{user.nickname}-{user.name}",
+    )
+
+
