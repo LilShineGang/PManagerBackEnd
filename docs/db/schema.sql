7c00170358c5ec45ba1988effeb0e7b4f778546e
LilShineGang <blagovestdoukovy2@gmail.com>
2025-11-19 16:33:46 +0100

    chore: add content to schema.sql

--- docs/db/schema.sql
+++ docs/db/schema.sql
@@ -1,1 +1,11 @@
 -- Aqui va a estar el contenido del schema.sql
+
+CREATE TABLE USERS(
+  id_user INT PRIMARY KEY AUTO_INCREMENT,
+  nickname VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  image VARCHAR(255),
+  UNIQUE()
+
+)
