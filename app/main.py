4db4a13a6bf4c30c14e8d986e87d46f47bdf5c61
Javier <javpomgom@alu.edu.gva.es>
2026-02-09 20:25:52 +0100

    feat: add forums and wiki endpoints

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
 
-from app.routers import users, games, forums
+from app.routers import users, games, forums, wiki
 
 app = FastAPI(debug=True)
 
 app.include_router(users.router)
 app.include_router(games.router)
 app.include_router(forums.router)
+app.include_router(wiki.router)
 
 @app.get("/")
 async def root():
