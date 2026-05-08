ad276ee8949ccc76b1a97abaececc4af97a6b3c0
Dandeline <blagovestdoukovy2@gmail.com>
2026-05-08 14:33:49 +0200

    Fix typo in 'gendre' to 'gender' in Game class

--- app/domain/entities/game.py
+++ app/domain/entities/game.py
@@ -6,7 +6,7 @@ from app.domain.entities.game_rating import GameRating
 class Game:
     id: Optional[int] = None
     name: str = ""
-    gendre: str = ""
+    gender: str = ""
     difficulty: str = ""
     rating: Optional[GameRating] = None
     image: Optional[str] = None
